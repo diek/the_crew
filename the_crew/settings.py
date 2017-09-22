316fe93fdaaca049c4ae2dca75d8be892711d5cf
derrick kearney <diek@bellaliant.net>
2017-09-22 20:24:08 -0300

    Make initial migration of user and related models

--- the_crew/settings.py
+++ the_crew/settings.py
@@ -76,6 +76,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'core_user.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
