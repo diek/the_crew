139c2f41314049379093d4301ae82041223a28a7
derrick kearney <diek@bellaliant.net>
2017-09-22 20:34:18 -0300

    Add driver class

--- core_user/models.py
+++ core_user/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -25,3 +27,24 @@ class User(AbstractUser):
     longitude = models.FloatField(blank=True, null=True)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
+
+
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    has_ignition_locking_auth = models.BooleanField(default=False)
+    license_issue_date = models.DateField(null=True, blank=True)
+    license_expiration_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        permissions = (("Can view driver info", "view_driver_info"),)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Driver.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
